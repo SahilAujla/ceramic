5821c7a10978f31873d9b1b2d0dfcbec689e3c6d
Sahil Aujla <sahilaujla15@gmail.com>
2022-03-22 12:42:54 +0530

    Fixed the hook error.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { Web3Provider } from "@ethersproject/providers";
 import { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-const web3ModalRef = useRef();
 import { useViewerConnection } from "@self.id/react";
 import { EthereumAuthProvider } from "@self.id/web";
 import { useViewerRecord } from "@self.id/react";
 
 export default function Home() {
+  const web3ModalRef = useRef();
   const getProvider = async () => {
     const provider = await web3ModalRef.current.connect();
     const wrappedProvider = new Web3Provider(provider);
